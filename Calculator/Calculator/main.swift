2233b855d1702f2830079ffd39790d864eed31cb
diclecoban <d.coban2022@gtu.edu.tr>
2025-08-26 02:27:47 +0300

    Calculation of History added

--- Calculator/Calculator/main.swift
+++ Calculator/Calculator/main.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+var results: [Double] = []
 menu()
 
 
@@ -16,7 +17,14 @@ func menu() {
     print("WELCOME TO SARA CALCULATOR")
     print("---------------------------")
     
-    while(true) {
+    print("Please make your choice")
+    print("1. Calculate",
+          "2. See Last Calculations",
+          "3. Exit")
+    let choice = Int(readLine()!)!
+    
+    switch choice {
+    case 1:
         print("Please enter your first number for calculation")
         let number1 = Double(readLine()!)!
         print("Please enter your second number for calculation")
@@ -25,6 +33,32 @@ func menu() {
         let operand = Character(readLine()!)
         
         calculation(number1: number1, number2: number2, operand: operand)
+    case 2:
+        print("Last Calculations:")
+        print(results)
+        ContinueOrNot()
+    case 3:
+        exit(0)
+    default:
+        print("invalid input")
+    }
+}
+
+func saveResults(result: Double) {
+    results.append(result)
+}
+
+func ContinueOrNot() {
+    print("Do you want to continue? (y/n)")
+    let input = readLine()
+    if input == "n" {
+        results.removeAll()
+        exit(0)
+    } else if input == "y" {
+        menu()
+    } else {
+        print("Invalid input")
+        exit(0)
     }
 }
 
@@ -32,27 +66,22 @@ func calculation(number1: Double, number2: Double, operand: Character){
     switch operand {
     case "+":
        let result = number1 + number2
+        saveResults(result: result)
         print(result)
     case "-":
        let result = number1 - number2
+        saveResults(result: result)
         print(result)
     case "*":
        let result = number1 * number2
+        saveResults(result: result)
         print(result)
     case "/":
        let result = number1 / number2
+        saveResults(result: result)
         print(result)
     default:
         print("Invalid Operand")
     }
-    
-    print("Do you want to continue? (y/n)")
-    if readLine() == "n" {
-        exit(0)
-    } else if readLine() == "y" {
-        menu()
-    } else {
-        print("Invalid input")
-        exit(0)
-    }
+    ContinueOrNot()
 }
